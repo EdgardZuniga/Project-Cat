cd3afd0966495ae997b325ddae6df8ff012a5199
Edgard Zuniga <edgard.zunigam@gmail.com>
2024-03-15 17:24:48 -0600

    Creacion de script para la automatizacion de enlaces de el Header-Nav y tambien de las imagenes del Footer.

---
+++ Modules/Footer-Config.js
@@ -0,0 +1,30 @@
+const images = {
+    Facebook : '/Resources/General-images/facebook-icon.png',
+    Twitter : '/Resources/General-images/twiter-icon.png',
+    Instagram : '/Resources/General-images/instagram-icon.png',
+    Linkedin : '/Resources/General-images/linkedin-icon.png',
+    WhatsApp : '/Resources/General-images/whatsapp-icon.png'
+}
+
+const Facebook = document.querySelector('#Facebook'),
+Twitter = document.querySelector('#Twitter'),
+Instagram = document.querySelector('#Instagram'),
+Linkedin = document.querySelector('#Linkedin'),
+WhatsApp = document.querySelector('#WhatsApp');
+
+
+      function addLink(element, name){
+        if (images[name]) {
+            element.setAttribute('src', images[name]);
+            element.setAttribute('alt', name + "-icon");
+        } else {
+            console.error(`Error: ${name} no se encuentra en el objeto de imagenes.`);
+        }
+    }
+    
+
+addLink(Facebook, "Facebook");
+addLink(Twitter, "Twitter");
+addLink(Instagram, "Instagram");
+addLink(Linkedin, "Linkedin");
+addLink(WhatsApp, "WhatsApp");
---
+++ Modules/pages-Script.js
@@ -0,0 +1,32 @@
+const Pages = {
+    Index: "/Index.html",
+    Behaviour: "/Pages/Behaviour/Behaviour.html",
+    Breeds: "/Pages/Breeds/Breeds.html",
+    Care: "/Pages/Care/Care.html",
+    Contact: "/Pages/Contact/Contact.html",
+    InterestingFacts: "/Pages/Interesting-Facts/interesting-facts.html"
+}
+
+const Index = document.querySelector('#Index'),
+      Breeds = document.querySelector('#Breeds'),
+      Care = document.querySelector('#Care'),
+      Behaviour = document.querySelector('#Behaviour'),
+      Contact = document.querySelector('#Contact'),
+      Interesting = document.querySelector('#Interesting');
+
+
+      function addLink(element, name){
+        if (Pages[name]) {
+            element.href = Pages[name];
+        } else {
+            console.error(`La página '${name}' no está definida en el objeto Pages.`);
+        }
+    }
+    
+
+addLink(Index, "Index");
+addLink(Breeds, "Breeds");
+addLink(Care, "Care");
+addLink(Behaviour, "Behaviour");
+addLink(Contact, "Contact");
+addLink(Interesting, "InterestingFacts");
