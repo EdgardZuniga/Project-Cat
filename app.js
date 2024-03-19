28b4fccc7ba7186bfa5a86b72eb917c2dafd62ba
Edgard Zuniga <edgard.zunigam@gmail.com>
2024-03-19 17:54:49 -0600

    Se corrigieron errores en en js de la raiz

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ addImg(WhatsApp, "WhatsApp");
 
 const Pages = {
     Index: "Index.html",
-    Contact: "Pages/Contacts/Contact.html",
+    Contact: "Pages/Contacts/Contact-me.html",
     Behaviour: "Pages/Behaviour/Behaviour.html",
     InterestingFacts: "Pages/Interesting-Facts/Interesting-Facts.html",
     Breeds: "Pages/Breeds/Breeds.html",
